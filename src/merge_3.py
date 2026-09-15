fd6674fa62e9c6e4b5054139f8330926f8e271ae
Hasan Al-Quaid <hasan.alquaid@gmail.com>
2026-09-15 19:59:58 -0400

    remove duplicate columns, and regenerated the mega df

--- src/merge_3.py
+++ src/merge_3.py
@@ -11,17 +11,35 @@ Merge 3 data frames into one big data frame
 from load_data import load_data
 from filter_motorcycle import filter_motorcycle_data
 
+def remove_duplicate_columns(df):
+    """
+    Remove duplicate columns in the three dataframes
+
+    Parameter:
+        df (pd.DataFrame): the mega dataframe, that has the  accident, person, and vehicle
+
+    Returns:
+        pd.DataFrame: the same dataset with duplicate columns dropped
+    """
+    duplicate_cols = df.T.duplicated()
+    print("Dropping duplicate columns:", list(df.columns[duplicate_cols]))
+    print("Number of columns dropped:", duplicate_cols.sum())
+    
+    return df.loc[:, ~duplicate_cols]
 
 def merge_pipeline():
     accident, person, vehicle, = load_data()
     motorcycles_accidents = filter_motorcycle_data(vehicle)  
-    #print("motorcycles_accidents:", motorcycles_accidents.shape)
+    print("motorcycles_accidents:", motorcycles_accidents.shape)
 
     merged = motorcycles_accidents.merge(accident, on="ST_CASE", how="left")
-    #print("After merging accident:", merged.shape)
+    print("After merging accident:", merged.shape)
 
     merged = merged.merge(person, on=["ST_CASE", "VEH_NO"], how="left")
-    #print("After merging person:", merged.shape)
+    print("After merging person:", merged.shape)
+
+    merged = remove_duplicate_columns(merged)
+    print("Removed Duplicates DF:", merged.shape)
 
     return merged
 
