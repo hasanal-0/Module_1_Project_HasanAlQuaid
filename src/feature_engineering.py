59d49b4c9ed5490eff7fe09d0e4f3f22771b216e
HasanAl-Quaid <hasan.alquaid@gmail.com>
2026-09-18 10:47:44 -0400

    clean file structrue

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -2,7 +2,9 @@
 AIPI 510: Module 1 Project: Data Storytelling
 - src/feature_engineering.py
 =================================================
-Engineer featrues that would me people better insight into the data.
+Engineer featrues that would give me better insight into the data.
+    - 
+    -
     - 
     -
 """
